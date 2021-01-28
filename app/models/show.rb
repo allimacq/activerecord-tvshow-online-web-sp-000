6257c4b7141ba55c8b2efd1411e0aa245839bd79
Allison _ <allimacq@gmail.com>
2021-01-28 04:38:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,5 +5,8 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
+    highest_rating = self.highest_rating
+    show = self.find_by(:rating => highest_rating)
+  end
     
 end
