56bce3117c9c87a7e64cff1f1a38141d9bd73ce3
Allison _ <allimacq@gmail.com>
2021-01-28 04:39:52 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,5 +11,9 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     self.minimum(:rating)
   end
+  
+  def self.least_popular_show
+    show = self.find_by(:rating => self.lowest_rating)
+  end
     
 end
